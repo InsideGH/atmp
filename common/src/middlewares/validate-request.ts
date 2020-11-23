72599abf93b6a5dd12584055dbe0bfb2cf935a95
Peter Larsson <peter.larsson@24hr.se>
2020-11-23 10:56:39 +0100

    Common updates

--- common/src/middlewares/validate-request.ts
+++ common/src/middlewares/validate-request.ts
@@ -6,7 +6,11 @@ export const validateRequest = (req: Request, res: Response, next: NextFunction)
   /**
    * Input validation.
    */
+  console.log('AAA', req.body);
   const errors = validationResult(req);
+  console.log('BBB', errors);
+  console.log('CCC', errors.isEmpty());
+
   if (!errors.isEmpty()) {
     throw new RequestValidationError(errors.array());
   }
