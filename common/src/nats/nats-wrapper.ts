dfb4d3cf2e368a930b996687c626232041689322
Peter Larsson <peter.larsson@24hr.se>
2020-11-23 10:15:41 +0100

    Common updates

--- common/src/nats/nats-wrapper.ts
+++ common/src/nats/nats-wrapper.ts
@@ -1,7 +1,6 @@
 import nats, { Stan } from 'node-nats-streaming';
-import { logger } from '../logger/pino';
 
-class Nats {
+class NatsWrapper {
   /**
    * This property might be undefined in between creation and connect call.
    */
@@ -26,22 +25,18 @@ class Nats {
    * @param url The url to Nats service.
    */
   connect(clusterId: string, clientId: string, url: string) {
-    logger.info('Connect to nats - ..');
-
     this._client = nats.connect(clusterId, clientId, { url });
 
     return new Promise((resolve, reject) => {
       this.client.on('connect', () => {
-        logger.info('Connect to nats - ok');
         resolve(this._client);
       });
 
       this.client.on('error', (err) => {
-        logger.info('Connect to nats - error');
         reject(err);
       });
     });
   }
 }
 
-export const natsWrapper = new Nats();
+export const natsWrapper = new NatsWrapper();
