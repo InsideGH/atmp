713139181a3a030c2cf74c0c3185e575212f520b
Peter Larsson <peterlarssonsmail@gmail.com>
2020-11-25 20:44:00 +0100

    Common updates

--- common/src/events/events/error-unknown-subject-event.ts
+++ common/src/events/events/error-unknown-subject-event.ts
@@ -4,8 +4,11 @@ import { BaseEvent } from '../base/base-event';
 export interface ErrorUnknownSubject extends BaseEvent {
   subject: Subjects.ErrorUnknownSubject;
   data: {
-    errorMessage: string,
-    serviceName: string,
-    event: BaseEvent
+    errorMessage: string;
+    serviceName: string;
+    unknownEvent: {
+      subject: string;
+      data: any;
+    };
   };
 }
