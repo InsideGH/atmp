33a211d4444ec4c13751a936b5418e0a7fdf11e9
Peter Larsson <peterlarssonsmail@gmail.com>
2020-11-26 16:58:46 +0100

    Common updates

--- common/src/events/base/any-publisher.ts
+++ common/src/events/base/any-publisher.ts
@@ -3,13 +3,22 @@ import { logger } from '../../logger/pino';
 import { AnyEvent } from './any-event';
 
 export class AnyPublisher {
+  name: string;
+
   /**
    * Create a nats streaming publisher.
    *
    * @param client Nats streaming client
    * @param enableDebugLogs Enable debug logs, requires LOG_LEVEL=debug
+   * @param publisherName Appends name to logs if exists
    */
-  constructor(protected client: Stan, protected enableDebugLogs: Boolean = false) {}
+  constructor(
+    protected client: Stan,
+    protected enableDebugLogs: Boolean = false,
+    private publisherName?: string,
+  ) {
+    this.name = publisherName ? `any-publisher-${publisherName}` : '';
+  }
 
   /**
    * Call this to send the event.
@@ -21,12 +30,16 @@ export class AnyPublisher {
       this.client.publish(event.subject, JSON.stringify(event.data), (err) => {
         if (err) {
           if (this.enableDebugLogs) {
-            logger.debug(`any-publisher: publishing error for event id=${event.data.id} subject=${event.subject}`);
+            logger.debug(
+              `${name}: publishing error for event id=${event.data.id} subject=${event.subject}`,
+            );
           }
           return reject(err);
         }
         if (this.enableDebugLogs) {
-          logger.debug(`any-publisher: sent event id=${event.data.id} subject=${event.subject} to nats`);
+          logger.debug(
+            `${name}: sent event id=${event.data.id} subject=${event.subject} to nats`,
+          );
         }
         return resolve();
       });
