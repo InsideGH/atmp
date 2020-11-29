111097c8d84b7ff54c089bb1c88793b7d6984d41
Peter Larsson <peterlarssonsmail@gmail.com>
2020-11-29 20:52:20 +0100

    Common updates

--- common/src/events/subjects.ts
+++ common/src/events/subjects.ts
@@ -2,4 +2,6 @@ export enum Subjects {
   ErrorEvent = 'error:event',
   PatientCreated = 'patient:created',
   PatientUpdated = 'patient:updated',
+  DeviceCreated = 'device:created',
+  DeviceUpdated = 'device:updated',
 }
