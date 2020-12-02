7cc3a20de5273ddb001c9b8221938896aab0c022
Peter Larsson <peterlarssonsmail@gmail.com>
2020-12-02 17:05:26 +0100

    System web with event and ajax table

--- system/src/routes/get-events.ts
+++ system/src/routes/get-events.ts
@@ -10,14 +10,52 @@ import {
 } from '@thelarsson/acss-common';
 import db from '../sequelize/database';
 import { models } from '../sequelize/models';
+import { Op } from 'sequelize';
 
 const router = express.Router();
 
-router.get('/', validateRequest, async (req: Request, res: Response) => {
-  const { body } = req;
-
-  const events = await models.Event.findAndCountAll({});
-  res.status(200).send(events);
+router.get('/subjects', validateRequest, async (req: Request, res: Response) => {
+  res.status(200).send(Object.values(Subjects).sort());
 });
+
+router.post(
+  '/events',
+  [body('offset').not().isEmpty().withMessage('offset is required')],
+  [body('limit').not().isEmpty().withMessage('limit is required')],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const {
+      body: { offset, limit, sorter = [], filters = [] },
+    } = req;
+
+    const order = sorter
+      .filter((x: any) => x.field && x.order)
+      .map((x: any) => [x.field, x.order == 'ascend' ? 'ASC' : 'DESC']);
+
+    console.log('filters', filters);
+
+    const where: any = {};
+
+    Object.keys(filters).forEach((key) => {
+      const columnName = key;
+      const values = filters[columnName];
+      if (values) {
+        where[columnName] = {
+          [Op.in]: values,
+        };
+      }
+    });
+
+    console.log('where', where);
+
+    const events = await models.Event.findAndCountAll({
+      where,
+      limit,
+      offset,
+      order,
+    });
+    res.status(200).send(events);
+  },
+);
 
 export { router as getEventsRoute };
