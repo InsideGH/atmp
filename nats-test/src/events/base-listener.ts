fd3e70ab0cff28e17b0b6c09194c8f5f28b3e93c
Peter Larsson <peterlarssonsmail@gmail.com>
2020-12-02 18:45:13 +0100

    Nats test
    simple non-docker non-kubernetes nats listener/publisher playground..
    
    requires that your nats is up and running though.

---
+++ nats-test/src/events/base-listener.ts
@@ -0,0 +1,58 @@
+/// <reference types="@types/node" />
+
+import { Message, Stan } from 'node-nats-streaming';
+import { Subjects } from './subjects';
+
+interface Event {
+  subject: Subjects;
+  data: any;
+}
+
+export abstract class Listener<T extends Event> {
+  abstract subject: T['subject'];
+  abstract queueGroupName: string;
+  abstract onMessage(data: T['data'], msg: Message): void;
+
+  private client: Stan;
+  protected ackWait: number = 5 * 1000;
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait)
+      .setDurableName(this.queueGroupName);
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions(),
+    );
+
+    this.client.on('error', () => {
+      console.log("#nats client on error");
+    });
+
+    subscription.on('message', async (msg: Message) => {
+      const parsedData = this.parseMessage(msg);
+      try {
+        await this.onMessage(parsedData, msg);
+      } catch (error) {
+        console.log('base-listener catched error');
+      }
+      // msg.ack();
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+    return typeof data === 'string' ? JSON.parse(data) : JSON.parse(data.toString('utf8'));
+  }
+}
---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,58 @@
+/// <reference types="@types/node" />
+
+import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
+// import { TicketCreatedEvent } from './events/ticket-created-events';
+// import { Subjects } from './events/subjects';
+
+console.clear();
+
+// stan is nats backwards, it's a client
+// abc is client id
+const stan = nats.connect('acss', 'abc', {
+  url: 'http://localhost:4222',
+});
+
+// const publishTicketCreated = (data: {
+//   id: string;
+//   title: string;
+//   price: number;
+// }): Promise<void> => {
+//   return new Promise((resolve, reject) => {
+//     stan.publish(Subjects.TicketCreated, JSON.stringify(data), (err) => {
+//       if (err) {
+//         return reject(err);
+//       }
+//       console.log('Event published to subject', Subjects.TicketCreated);
+//       return resolve();
+//     });
+//   });
+// };
+
+stan.on('connect', async () => {
+  console.log('publisher connected to NATS');
+
+  const publisher = new TicketCreatedPublisher(stan);
+
+  // await publishTicketCreated({
+  //   id: '123',
+  //   title: 'concert',
+  //   price: 20,
+  // });
+
+  await publisher.publish({
+    id: '123',
+    title: 'concert',
+    price: new Date().getTime(),
+  });
+
+  // const data = JSON.stringify({
+  //   id: '123',
+  //   title: 'concert',
+  //   price: 20,
+  // });
+
+  // stan.publish('ticket:created', data, () => {
+  //   console.log('Event published');
+  // });
+});
