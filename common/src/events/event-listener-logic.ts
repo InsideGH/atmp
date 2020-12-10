0c5c0dbdd67aecdff540fdba677497bbda6e59fd
Peter Larsson <peterlarssonsmail@gmail.com>
2020-12-10 09:25:22 +0100

    Common updates

--- common/src/events/event-listener-logic.ts
+++ common/src/events/event-listener-logic.ts
@@ -9,19 +9,10 @@ export class EventListenerLogic {
     /**
      * OUT OF ORDER
      *
-     * Update event happens BEFORE create event. We must NO_ACK, to process the event later.
+     * Update event happens BEFORE create event. We NO_ACK, to process the event later.
      */
     if (!curr) {
       return Decision.NO_ACK;
-    }
-
-    /**
-     * DUPLICATION OF EVENTS
-     *
-     * The event version is older or equal to the database entry. It must be a duplicated event.
-     */
-    if (event.versionKey <= curr.versionKey) {
-      return Decision.ACK;
     }
 
     /**
@@ -32,7 +23,17 @@ export class EventListenerLogic {
     }
 
     /**
-     * The event version is two or more steps ahead of the database entry. We NO_ACK, to process the event later.
+     * DUPLICATION OF EVENTS
+     *
+     * The event version is older or equal to the database entry. It must be a dup event.
+     */
+    if (event.versionKey <= curr.versionKey) {
+      return Decision.ACK;
+    }
+
+    /**
+     * The event version is two or more steps ahead of the database entry.
+     * We NO_ACK, to process the event later.
      */
     return Decision.NO_ACK;
   }
