e8c46b6cbb22d4000a851b4d547f61d12062797c
Peter Larsson <peterlarssonsmail@gmail.com>
2020-12-23 16:18:26 +0100

    Common updates

--- common/src/logger/pino.ts
+++ common/src/logger/pino.ts
@@ -10,5 +10,10 @@ const prettyPrint = enablePretty
 
 export const logger = pino({
   level: process.env.LOG_LEVEL || 'debug',
-  prettyPrint,
+  prettyPrint :false,
 });
+
+export const reqLogger = logger.child({ domain: 'API' });
+
+export const eventLogger = logger.child({ domain: 'EVENT' });
+
