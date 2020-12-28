742eb6e44b9ef353be309eb6b5c537d1d66d71f0
Peter Larsson <peterlarssonsmail@gmail.com>
2020-12-28 22:00:23 +0100

    Common updates

--- common/src/logger/pino.ts
+++ common/src/logger/pino.ts
@@ -2,7 +2,7 @@ import pino from 'pino';
 
 export const logger = pino({
   level: process.env.LOG_LEVEL || 'debug',
-  prettyPrint :false,
+  prettyPrint: false,
 });
 
 export const apiLogger = logger.child({ domain: 'API' });
@@ -11,3 +11,4 @@ export const eventLogger = logger.child({ domain: 'EVENT' });
 
 export const systemLogger = logger.child({ domain: 'SYSTEM' });
 
+export const clientWebAdminLogger = logger.child({ domain: 'CLIENT_WEBADMIN' });
